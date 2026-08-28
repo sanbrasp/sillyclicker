7db633d0fc63632563f4b4685bba938f613aac8e
Sandra Nilsen Brattli-Aspvik <sandrabrattliaspvik@gmail.com>
2026-08-28 21:46:57 +0200

    add StateEnumerator to directory Traversals

--- Traversals/StateEnumerator.cs
+++ Traversals/StateEnumerator.cs
@@ -1,6 +1,35 @@
-﻿namespace SillyClicker.Traversals;
+﻿using SillyClicker.Domain;
+
+namespace SillyClicker.Traversals;
 
 internal class StateEnumerator
 {
-    
+    internal List<GameState> EnumerateAllStates(GameLayout layout, string layoutId)
+    {
+        var results = new List<GameState>();
+        var queue =  new Queue<GameState>();
+        
+        queue.Enqueue(GameState.Initial(layoutId));
+
+        while (queue.Count > 0)
+        {
+            var current = queue.Dequeue();
+            results.Add(current);
+
+            if (current.IsLoss) //TODO: fix IsWin logic
+            {
+                continue;
+            }
+            
+            for (int i = 0; i < layout.BoxCount; i++)
+            {
+                if (!current.IsBoxClicked(i))
+                {
+                    var next = current.ApplyClick(layout, i);
+                    queue.Enqueue(next);
+                }
+            }
+        }
+        return results;
+    }
 }
