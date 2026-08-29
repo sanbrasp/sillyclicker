782f596e6b6b6391ad2a79720ce64c55ac8d4b94
Sandra Nilsen Brattli-Aspvik <sandrabrattliaspvik@gmail.com>
2026-08-29 16:25:41 +0200

    add SvgRenderer

---
+++ Rendering/SvgRenderer.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using SillyClicker.Domain;
+
+namespace SillyClicker.Rendering;
+
+internal class SvgRenderer
+{
+    internal string Render(GameLayout layout, GameState gameState)
+    {
+        var sb = new StringBuilder();
+
+        sb.Append("<svg width=\"600\" height=\"200\" xmlns=\"http://www.w3.org/2000/svg\">");
+
+        for (int i = 0; i < layout.BoxCount; i++)
+        {
+            var box = layout.GetBox(i);
+            
+            // TODO: decide on a fill color based on gameState.IsBoxClicked(i)
+            // something like: string fill = ?? ? "some color" : "some other color";
+            string clickedBoxColor = "green";
+            string unclickedBoxColor = "gray";
+            
+            string fill = gameState.IsBoxClicked(i) ? clickedBoxColor : unclickedBoxColor;
+            
+            sb.Append(
+                $"<rect x=\"{box.X}\" y=\"{box.Y}\" width=\"{box.Width}\" height=\"{box.Height}\" fill=\"{fill}\" />");
+        }
+        sb.Append("</svg>");
+        return sb.ToString();
+    }
+}
